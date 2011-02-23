875f53f3572b25285443722ec7caab4ee6009fae
Adam Keys <adam@therealadam.com>
2011-02-22 20:06:28 -0600

    Unslime skeleton classes.

--- lib/sifter.rb
+++ lib/sifter.rb
@@ -8,8 +8,40 @@ class Sifter
   autoload :Account, "sifter/account"
   autoload :Project, "sifter/project"
 
-  # SLIME
-  class Sifter::Issue < Hashie::Mash; end
-  class Sifter::Milestone < Hashie::Mash; end
-  class Sifter::Person < Hashie::Mash; end
+  class Issue < Hashie::Dash
+
+    property :number
+    property :subject
+    property :description
+    property :priority
+    property :status
+    property :assignee_name
+    property :category_name
+    property :milestone_name
+    property :opener_name
+    property :url
+    property :api_url
+    
+  end
+
+  class Milestone < Hashie::Dash
+    
+    property :name
+    property :due_date
+    property :url
+    property :api_url
+    property :issues_url
+    property :api_issues_url
+    
+  end
+
+  class Person < Hashie::Dash
+    
+    property :username
+    property :first_name
+    property :last_name
+    property :email
+    
+  end
+
 end
